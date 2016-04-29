5c50e7b04b58d3b1fd8e2f292229e768f0859d9d
Benjamin <benjaradona@gmail.com>
2016-04-29 23:12:22 +0200

    gulp tasks like style, inject and serve

--- library/gulpfile.js
+++ library/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
@@ -15,12 +16,33 @@ gulp.task('style', function () {
 
 gulp.task('inject', function () {
     var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], {read: false});
+    var injectoptions = {
+        ignorePath: '/public'
+    };
     var options = {
         bowerJson: require('./bower.json'),
         directory: './public/lib',
         ignorePath: '../../public'
-    }
+    };
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
-        .pipe(gulp.dest('./src/views'))
-})
+        .pipe(inject(injectSrc, injectoptions))
+        .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve',['style','inject'], function () {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 3000
+        },
+        watch: jsFiles
+    };
+    return nodemon(options)
+        .on('restart', function (ev) {
+            console.log('Restarting....');
+        });
+});
