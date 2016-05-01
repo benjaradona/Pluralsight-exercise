b3ce3fe387b0420355d69ad5a32b23f4a07f62c7
Benjamin <benjaradona@gmail.com>
2016-05-01 16:52:10 +0200

    Routing, ejs-automation etc

--- library/app.js
+++ library/app.js
@@ -2,11 +2,58 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 
+var bookRouter = express.Router();
+
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+
+app.set('views', './src/views');
+
+app.set('view engine', 'ejs');
+
+var books = [
+    {
+        title: 'Born to Run',
+        genre: 'Science'
+    },
+    {
+        title: 'War and Peace',
+        genre: 'Historical Fiction'
+    },
+    {
+        title: 'Les Miserable',
+        genre: 'Historical Fiction'
+    }
+];
+
+bookRouter.route('/')
+    .get(function(req, res) {
+        res.render('books', {
+        title: 'Books',
+        nav: [{
+            Link: '/Books', Text: 'Books'
+        }, {
+            Link: '/Authors', Text: 'Authors'
+            }],
+        books: books
+    });
+});
+
+bookRouter.route('/single')
+    .get(function(req, res) {
+        res.send('Hello Single Book');
+    });
+
+app.use('/Books', bookRouter);
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [{
+            Link: '/Books', Text: 'Books'
+        }, {
+                Link: '/Authors', Text: 'Authors'
+            }]
+    });
 });
 
 app.get('/books', function (req, res) {
